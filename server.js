a5f838b3c023c26a6789f251dde53eb2d0afe491
mong-kieu1807 <0306231397@caothang.edu.vn>
2026-05-01 00:19:56 +0700

    Tạo dự án

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Library Management System API is running...');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
